07b7cf0da3d3339c94e0a67a3fefefdc1ed086f0
arish <arishsultan104@gmail.com>
2021-01-19 13:18:32 +0500

    :heavy_plus_sign: Added Items Distribution Table

--- lib/src/db/items-distribution_table.dart
+++ lib/src/db/items-distribution_table.dart
@@ -2,5 +2,12 @@ import 'package:moor/moor.dart';
 
 class ItemDistributionTable extends Table {
   IntColumn get id => integer().autoIncrement()();
-  // IntColumn get item => integer().customConstraint('NOT NULL REFERENCES items(id)')();
+
+  IntColumn get item =>
+      integer().customConstraint('NOT NULL REFERENCES items(id)')();
+
+  IntColumn get distributor =>
+      integer().customConstraint('NOT NULL REFERENCES distributors(id)')();
+
+  RealColumn get price => real()();
 }
