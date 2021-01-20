bae34727ce2717a2c6735a14dc63f99652e58fca
arish <arishsultan104@gmail.com>
2021-01-20 11:54:01 +0500

    Removed Padding from FancyTile.

--- lib/src/ui/widgets/fancy_tile.dart
+++ lib/src/ui/widgets/fancy_tile.dart
@@ -9,26 +9,23 @@ class FancyTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade300,
-              spreadRadius: 1,
-              blurRadius: 10,
-            ),
-          ],
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: ListTile(
-          title: title,
-          onTap: onTap,
-          leading: leading,
-          contentPadding: EdgeInsets.fromLTRB(15, 5, 15, 5),
-        ),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            spreadRadius: 1,
+            blurRadius: 10,
+          ),
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        title: title,
+        onTap: onTap,
+        leading: leading,
+        contentPadding: EdgeInsets.fromLTRB(15, 5, 15, 5),
       ),
     );
   }
