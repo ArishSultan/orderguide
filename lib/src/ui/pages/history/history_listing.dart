85a214ae20b0fd05e2d92242479b800209e16d32
arish <arishsultan104@gmail.com>
2021-01-21 16:05:37 +0500

    Implemented Completely

--- lib/src/ui/pages/history/history_listing.dart
+++ lib/src/ui/pages/history/history_listing.dart
@@ -1,82 +1,94 @@
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:orderguide/src/base/db.dart';
 import 'package:orderguide/src/base/nav.dart';
+import 'package:orderguide/src/models/order.dart';
+import 'package:orderguide/src/ui/views/item_search_view.dart';
 
 import 'order-detail_page.dart';
 
 class HistoryListing extends StatefulWidget {
+  final bool completed;
+
+  HistoryListing(this.completed);
+
   @override
   _HistoryListingState createState() => _HistoryListingState();
 }
 
 class _HistoryListingState extends State<HistoryListing> {
+  final controller = ItemSearchViewController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Orders History"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-           HistoryTile(),
-           HistoryTile(),
-           HistoryTile(),
-          ],
-        ),
-      ),
-    );
-  }
-}
+      body: ItemSearchView<Order>(
+        allowSearch: false,
+        onFetch: ([str]) => AppDB().getAllOrders(completed: widget.completed),
+        controller: controller,
+        builder: (context, order, _) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
+            child: InkWell(
+              onTap: () async {
+                await AppNavigation.to(
+                  context,
+                  OrderDetailPage(order, !widget.completed),
+                );
 
-
-class HistoryTile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: InkWell(
-        onTap: ()=> AppNavigation.to(context, OrderDetailPage()),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                      color: Colors.grey.shade200
-                  )
-                ],
-                borderRadius: BorderRadius.circular(10),
-              ),
+                controller.refresh();
+              },
               child: Column(
                 children: [
-                  ListTile(
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                          color: Colors.grey.shade200,
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
                       children: [
-                        Text("Panos Foods"),
-                        Text("18, January 2021",style: TextStyle(
-                          color: Colors.grey
-                        ),),
+                        ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(order.distributor.name ?? 'asd'),
+                              Text(
+                                DateFormat('dd, MMM y').format(order.createdAt),
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          subtitle: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('${order.count} ${order.count == 1 ? 'Item' : 'Items'}'),
+                              Text("\$${order.price.toStringAsFixed(2)}"),
+                            ],
+                          ),
+                          trailing: Icon(
+                            CupertinoIcons.chevron_right,
+                            color: Color.fromRGBO(75, 68, 206, 1),
+                          ),
+                        )
                       ],
                     ),
-                    subtitle: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("16 Items"),
-                        Text("\$100.00"),
-                      ],
-                    ),
-                    trailing:  Icon(Icons.chevron_right,color: Color.fromRGBO(75, 68, 206, 1),size: 35,),
-                  )
-
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
