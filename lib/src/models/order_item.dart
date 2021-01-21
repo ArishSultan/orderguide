6fd2f08087f60e91b3e48d47f5b407cc21b33bec
arish <arishsultan104@gmail.com>
2021-01-21 12:23:29 +0500

    :zap: Synced [OrderItem] fields with [OrderItemsTable]

--- lib/src/models/order_item.dart
+++ lib/src/models/order_item.dart
@@ -3,12 +3,14 @@ import 'package:orderguide/src/models/item.dart';
 class OrderItem extends Item {
   int quantity;
   double price;
+  bool completed;
 
   OrderItem({
     int id,
     String name,
     this.quantity,
     this.price,
+    this.completed,
   }) : super(
           id: id,
           name: name,
